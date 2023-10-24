4325673781c48d4df4c11e542a572abf2393919c
jeff kamuthu <kamuthuj@gmail.com>
2023-10-24 23:09:40 +0300

    Table values

---
+++ customer details..sql
@@ -0,0 +1,18 @@
+
+INSERT INTO customers (customer_id,customer_fname,customer_lname)
+VALUES(1,"James","Long"),
+       (2,"Grace","Piquet"),
+       (3,"Alex","Njue"),
+       (4,"Arthur","Njoroge"),
+       (5,"Emmy","Wanyoike"),
+       (6,"Eben","Etzebeth"),
+       (7,"Siya","Kolisi"),
+       (8,"Micheal","Johnson"),
+       (9,"Esther","Kanama"),
+       (10,"Mike","Epps"),
+       (11,"Billy","Kamau"),
+       (12,"Judy","Seret"),
+       (13,"Lewis","Kori"),
+       (14,"Teddy","Kihiu"),
+       (15, "Richard","Doe")
+       
---
+++ employees details..sql
@@ -0,0 +1,8 @@
+INSERT INTO employees(employee_id,employee_fname,employee_lname,employee_position,hourly_pay)
+VALUES(1,"Martha","Stewart","Manager",20.85),
+      (2,"John","Smith","Assistant",17.50),
+      (3,"Andrew","Njue","T.member", 14.60),
+      (4, "Grace","Shahidi","T.member",14.60),
+      (5, "Robert","Andre","T.member",14.60),
+      (6,"Jeff","Kamuthu","Intern",5.50),
+      (7,"Rodgers","Adiop","Intern",5.50)
